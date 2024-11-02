34350ba14dc5a29d9a93fc17c681089b2f0898eb
majomajo112 <patryk.pinczynski@o2.pl>
2024-11-02 15:51:38 +0100

    Update settings.py final  oby

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://patrykpinczy-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://patrykpinczy-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai'
 ]
+
 CSRF_TRUSTED_ORIGINS = [
-    'https://patrykpinczy-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://patrykpinczy-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -96,8 +99,10 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
         'NAME':
